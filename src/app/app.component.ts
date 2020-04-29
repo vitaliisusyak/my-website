1b3aec35100b4eebad4bfc1f348a9f2c6a1df7c1
Vitalii Susiak <vsusi@softserveinc.com>
2020-04-29 15:46:20 +0300

    initial colors added, home page finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ViewChild, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  /*encapsulation: ViewEncapsulation.None*/
 })
 export class AppComponent {
   title = 'my-website';
